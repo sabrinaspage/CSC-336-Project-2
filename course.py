d7d09d348ef5e8b20960f99c3e7d7abde3a4f723
ander9991 <60303192+ander9991@users.noreply.github.com>
2021-12-05 16:05:50 -0500

    Delete course.py

--- course.py
+++
@@ -1,7 +0,0 @@
-class course:
-    def __init__(self, courseID, courseName, instructor, numOfStudents, Semester):
-        self.courseID = courseID
-        self.courseName = courseName
-        self.instructor = instructor
-        self.numOfStudents = numOfStudents
-        self.Semester = Semester
