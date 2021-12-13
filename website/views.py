c87c04bc675a8d787cad66f634ed78e7c6bd2fac
Carlos Flores <carlos9915ok@gmail.com>
2021-12-13 00:58:38 -0500

    fixed login issue

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, url_for, redirect
 from flask_login import login_required, current_user
 from . import db
 from sqlalchemy import desc
@@ -15,12 +15,11 @@ views = Blueprint('views', __name__)
 def homepage():
     headings = ("Title", "", "Genre", "Episodes", "Score", "More Info")
     animes = Homepage.query.order_by(desc(Homepage.score)).all()
-
     if request.method == "POST":
         id = request.form.get("add")
         anime = Homepage.query.get(id) # got the anime
         new_entry = MyList(uid=anime.uid, title=anime.title, img_url=anime.img_url, score=anime.score, myscore="10", mycomment = "good")
         db.session.add(new_entry)
         db.session.commit()
-        return redirect(url_for('views.homepage', user=current_user, table_headings=headings, data=coursesData))
+        return redirect(url_for('views.homepage', user=current_user, table_headings=headings, data=animes))
     return render_template("homepage.html", user=current_user, table_headings=headings, data=animes)
